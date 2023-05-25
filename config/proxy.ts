035230216150fe4a7ea927bf071ba22fd5a4fd94
周必川 <zhoubichuan@icloud.com>
2023-05-25 22:26:34 +0800

    update

--- config/proxy.ts
+++ config/proxy.ts
@@ -9,7 +9,7 @@
 export default {
   dev: {
     '/api/': {
-      target: 'https://preview.pro.ant.design',
+      target: 'http://localhost:7001/',
       changeOrigin: true,
       pathRewrite: { '^': '' },
     },
